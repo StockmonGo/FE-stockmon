c567f13bb3fb20c7c6c7023bf803166be3fa6150
soyeon <uyyu1734@gmail.com>
2024-08-22 13:13:12 +0900

    design: 데브환경 레이아웃 조정

--- src/app/dev/layout.tsx
+++ src/app/dev/layout.tsx
@@ -11,11 +11,19 @@ export default function DevLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="p-4">
-      <div>
-        <h2>[개발용 페이지] 필요한 컴포넌트 여기서 테스트</h2>
+    <div className="w-full h-full overflow-x-hidden overflow-y-scroll">
+      <div className="relative w-full h-full overflow-hidden bg-stock-lemon-100 z-0">
+        <div
+          className="bg-contain bg-center w-full h-full fixed rotate-45 scale-150 bg-stock-lemon-100 z-[-1]"
+          style={{ backgroundImage: "url('/images/bg.png')" }}
+        ></div>
       </div>
-      {children}
+      <div className="p-4 max-w-xl min-w-96 h-64">
+        <div>
+          <h2>[개발용 페이지] 필요한 컴포넌트 여기서 테스트</h2>
+        </div>
+        {children}
+      </div>
     </div>
   );
 }
