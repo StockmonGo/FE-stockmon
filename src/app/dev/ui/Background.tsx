19da8ebbfab8267863df70c5e83bb393c46b673b
soyeon <uyyu1734@gmail.com>
2024-08-22 11:42:45 +0900

    feat: 테스트용 백그라운드 애니메이션

---
+++ src/app/dev/ui/Background.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./background.css";
+export default function Background() {
+  return (
+    <div className="w-full">
+      <div className="box">
+        <div className="wave -one"></div>
+        <div className="wave -two"></div>
+        <div className="wave -three"></div>
+        <div className="wave -four"></div>
+        <div className="title">Capacities</div>
+      </div>
+    </div>
+  );
+}
