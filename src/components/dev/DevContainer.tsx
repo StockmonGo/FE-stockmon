5894fc49060efd85d3c0850e8f8dab09ffef1ab9
soyeon <uyyu1734@gmail.com>
2024-08-22 11:45:41 +0900

    style: 공용 tailwind 스타일 묶어서 뺌

--- src/components/dev/DevContainer.tsx
+++ src/components/dev/DevContainer.tsx
@@ -6,8 +6,8 @@ type Props = {
 };
 export default function DevContainer(props: Props) {
   return (
-    <div className="py-2 border-solid border-1 border-stock-dark-200 rounded-lg">
-      <p>{props.title}</p>
+    <div className="py-2 border border-stock-dark-200 rounded-lg px-2">
+      <p className="text-lg font-semibold pb-2">{props.title}</p>
       {props.children}
     </div>
   );
