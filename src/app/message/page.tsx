f0e7a6ffe2338b3a058c9eb17a6ba08b5ccc442a
soyeon <uyyu1734@gmail.com>
2024-08-23 10:17:16 +0900

    feat:  알림창의 알림 리스트용 아이템 컴포넌트 추가
    MessageItem 컴포넌트 생성
    각각의 메세지용 컴포넌트
    확인&취소 버튼 이미지파일 추가
    notices 더미데이터 추가

--- src/app/message/page.tsx
+++ src/app/message/page.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import MessageItem from "./components/MessageItem";
 
+//알림이 있을경우&없을경우 + 팝업
+//TODO: 더미데이터 만들어서 띄우기. 없을경우 없는 UI 노출
+//type:1 동맹 type:2 스톡몬 교환
 export default function Message() {
-  return <div>메세지함</div>;
+  return (
+    <div className="w-full bg-stock-blue-200 border-4 border-dashed border-stock-border min-h-56 rounded-lg p-3">
+      <div className="w-full bg-stock-lemon-50  min-h-56 rounded-lg p-2">
+        메세지함
+        <MessageItem />
+      </div>
+    </div>
+  );
 }
