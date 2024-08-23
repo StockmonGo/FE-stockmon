107a2f7eb43ad7c70a7183f7ccc44339782bbed7
KimRiun <kimriun27@gmail.com>
2024-08-23 10:16:24 +0900

    feat: 여러 모달 UI 샘플을 확인할 수 있는 버튼 추가

--- src/app/dev/ui/page.tsx
+++ src/app/dev/ui/page.tsx
@@ -6,7 +6,9 @@ import Background from "./Background";
 import Modal from "@/components/ui/Modal";
 
 export default function UI() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen1, setIsModalOpen1] = useState(false);
+  const [isModalOpen2, setIsModalOpen2] = useState(false);
+  const [isModalOpen3, setIsModalOpen3] = useState(false);
 
   return (
     <div>
@@ -22,8 +24,33 @@ export default function UI() {
         <Background />
       </DevContainer>
       <DevContainer title="모달">
-        <Button text="모달 열기" click={() => setIsModalOpen(true)} />
-        <Modal open={isModalOpen} onClose={() => setIsModalOpen(false)}></Modal>
+        <div>
+          <Button
+            text="모달 1 : 닫기, 확인"
+            click={() => setIsModalOpen1(true)}
+          />
+          <Modal
+            open={isModalOpen1}
+            onClose={() => setIsModalOpen1(false)}
+            hasClose
+          ></Modal>
+        </div>
+        <div>
+          <Button text="모달 2 : 확인" click={() => setIsModalOpen2(true)} />
+          <Modal
+            open={isModalOpen2}
+            onClose={() => setIsModalOpen2(false)}
+          ></Modal>
+        </div>
+        <div>
+          <Button
+            text="모달 3 : children 삽입"
+            click={() => setIsModalOpen3(true)}
+          />
+          <Modal open={isModalOpen3} onClose={() => setIsModalOpen3(false)}>
+            <div>자식 컴포넌트</div>
+          </Modal>
+        </div>
       </DevContainer>
     </div>
   );
--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -4,26 +4,34 @@ type VARIANT = "blue" | "white";
 
 type Props = {
   open: boolean;
+  hasClose?: boolean;
   onClose: () => void;
   children?: React.ReactNode;
 };
-export default function Modal({ open, onClose, children }: Props) {
+export default function Modal({
+  open,
+  hasClose = false,
+  onClose,
+  children,
+}: Props) {
   return (
     open && (
       <div
         className="fixed inset-0 bg-stock-dark-950 bg-opacity-20 flex justify-center items-center z-50"
         // onClick={onClose}
       >
-        <div className=" flex flex-col gap-4 bg-gradient-to-b from-[#F9F0FF] to-[#E8F7FF] p-4 min-w-[360px] rounded-lg">
-          <header className="lex flex-col gap-2 py-3 bg-white text-center rounded-lg">
-            <h2 className=" font-bold">제목</h2>
-            <p className="text-sm">내용</p>
+        <div className="flex flex-col gap-4 justify-center items-center bg-gradient-to-b from-[#F9F0FF] to-[#E8F7FF] p-4 min-w-[360px] rounded-lg">
+          <header className="w-full flex flex-col gap-2 py-3 bg-white text-center rounded-lg">
+            <h2 className="text-lg font-bold">제목</h2>
+            <p>내용</p>
           </header>
           {children && children}
           <footer className="flex justify-center gap-16">
-            <button onClick={onClose}>
-              <img src="/icons/ButtonCloseActiveOn.svg" alt="close" />
-            </button>
+            {hasClose && (
+              <button onClick={onClose}>
+                <img src="/icons/ButtonCloseActiveOn.svg" alt="close" />
+              </button>
+            )}
             <button onClick={onClose}>
               <img src="/icons/ButtonOkActiveOn.svg" alt="ok" />
             </button>
