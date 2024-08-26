57e19d88e92f81263f30a9ab7dde2477f6465a26
KimRiun <kimriun27@gmail.com>
2024-08-26 11:23:34 +0900

    feat: 도감 페이지에 제목과 하단닫기 버튼이 있는 공통 레이아웃 적용 및 로딩시 useSWR 사용

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,26 +1,67 @@
+"use client";
 import CountHeader from "@/components/ui/books/CountHeader";
 import CommonLayout from "@/components/ui/CommonLayout";
-import React from "react";
+import { Skeleton } from "@/components/ui/Skeleton";
+import React, { useState } from "react";
+import useSWR from "swr";
+
+type StockmonItem = {
+  id: number;
+  name: string;
+  imgUrl: string;
+  count: number;
+  stockCode: number;
+  stockAveragePrice: number;
+};
+
+const dummy: { stockmons: StockmonItem[] } = {
+  stockmons: [
+    {
+      id: 1,
+      name: "신한지주몬",
+      imgUrl: "http~",
+      count: 10,
+      stockCode: 124125,
+      stockAveragePrice: 145400,
+    },
+    {
+      id: 2,
+      name: "하나금융지주몬",
+      imgUrl: "http~",
+      count: 12,
+      stockCode: 141125,
+      stockAveragePrice: 235400,
+    },
+  ],
+};
+
+const fetcher = (url: string) => {
+  // TODO: api 요청
+  return dummy;
+};
 
 export default function Books() {
-  const books = [
-    { id: 1, title: "Stockmon 1" },
-    { id: 2, title: "Stockmon 2" },
-    { id: 3, title: "Stockmon 3" },
-  ];
+  const { data, error } = useSWR("/api/user", fetcher);
 
+  if (error) return <div>failed to load</div>;
+  if (!data)
+    return (
+      <CommonLayout title={"도감"} header={<CountHeader isLoading />}>
+        <div></div>
+      </CommonLayout>
+    );
   return (
     <CommonLayout
       title={"도감"}
       header={
         /* TODO: api 연결 */
-        <CountHeader count={1000} />
+        <CountHeader count={data.stockmons.length} />
       }
     >
       <ul>
-        {books.map((book) => (
-          <li key={book.id}>
-            <a href={`/books/${book.id}`}>{book.title}</a>
+        {data.stockmons.map((stockmon) => (
+          <li key={stockmon.id}>
+            <a href={`/books/${stockmon.id}`}>{stockmon.name}</a>
           </li>
         ))}
       </ul>
--- src/components/ui/books/CountHeader.tsx
+++ src/components/ui/books/CountHeader.tsx
@@ -1,26 +1,23 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Skeleton } from "../Skeleton";
 
-type Props = {
-  count: number;
-};
+type Props =
+  | { isLoading: true; count?: never }
+  | { isLoading?: false; count: number };
 
-export default function CountHeader({ count }: Props) {
+export default function CountHeader({ isLoading, count }: Props) {
   const STOCKMON_MAX = 200;
-  const [stockmonCount] = useState<number>(count);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    if (stockmonCount) {
-      setIsLoading(false);
-    }
-  }, [stockmonCount]);
 
   return (
     <div className="w-full h-full flex justify-center items-center font-ptb text-2xl">
-      {isLoading && <Skeleton className="w-1/2 h-[30px]" />}
-      {!isLoading && `${stockmonCount}/${STOCKMON_MAX}`}
+      {isLoading ? (
+        <Skeleton className="w-1/2 h-[30px]" />
+      ) : (
+        <>
+          {count}/{STOCKMON_MAX}
+        </>
+      )}
     </div>
   );
 }
