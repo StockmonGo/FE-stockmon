f70c32c424d342cdeadf6d193e5a3d980febd271
hyuna <kook980316@gmail.com>
2024-08-26 09:04:01 +0900

    design: 각각의 icon 추가

--- src/components/ui/world/BottomNavBar.tsx
+++ src/components/ui/world/BottomNavBar.tsx
@@ -1,11 +1,19 @@
 import { BsPersonCircle } from "react-icons/bs";
+import { BsBackpack } from "react-icons/bs";
+import { IoPeopleCircleOutline } from "react-icons/io5";
 import NavButton from "./NavButton";
 
 export default function BottomNavBar() {
   return (
     <div className="w-screen max-w-xl min-h-16">
-      <NavButton onClick={() => {}}>
+      <NavButton type="profile">
         <BsPersonCircle />
+      </NavButton>
+      <NavButton type="books">
+        <BsBackpack />
+      </NavButton>
+      <NavButton type="alliances">
+        <IoPeopleCircleOutline />
       </NavButton>
     </div>
   );
