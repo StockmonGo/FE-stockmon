55950bedeaf2f799e3843f91c3bec12b5e500ed0
soyeon <uyyu1734@gmail.com>
2024-08-26 16:22:42 +0900

    feat: message 페이지 공용레이아웃으로 수정

--- src/app/message/layout.tsx
+++ src/app/message/layout.tsx
@@ -1,4 +1,4 @@
-import BtnClose from "@/components/ui/BtnClose";
+import CommonLayout from "@/components/ui/CommonLayout";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -11,25 +11,5 @@ export default function MessageLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <div className="w-full h-full overflow-x-hidden overflow-y-scroll">
-      <div className="fixed w-full h-full overflow-hidden z-0">
-        <div
-          className="bg-cover bg-center w-full h-full fixed z-[-1]"
-          style={{ backgroundImage: "url('/images/bg.jpg')" }}
-        ></div>
-      </div>
-      <div className="p-6 max-w-xl w-xl h-screen relative z-1 m-auto flex flex-col items-center justify-between gap-6">
-        <div className="header">
-          <h2 className="text-center text-stock-dark-800 text-xl font-bold py-2">
-            알림
-          </h2>
-        </div>
-        <div className="main flex-1 w-full overflow-hidden">{children}</div>
-        <div className="footer">
-          <BtnClose />
-        </div>
-      </div>
-    </div>
-  );
+  return <CommonLayout title={"알림"}>{children}</CommonLayout>;
 }
