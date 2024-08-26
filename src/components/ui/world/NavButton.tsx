2941cdbf3bc5c33602a9b766652de5c6af833fe7
hyuna <kook980316@gmail.com>
2024-08-26 09:03:41 +0900

    feat: onClick에 router.push 추가

--- src/components/ui/world/NavButton.tsx
+++ src/components/ui/world/NavButton.tsx
@@ -1,11 +1,14 @@
+import { useRouter } from "next/navigation";
+
 type Props = {
-  onClick: () => void;
   children?: React.ReactNode;
+  type: "books" | "alliances" | "profile";
 };
 
-export default function NavButton({ onClick: click, children }: Props) {
+export default function NavButton({ children, type }: Props) {
+  const router = useRouter();
   return (
-    <button className={`flex justify-center items-center w-1/3 min-h-16`} onClick={click}>
+    <button className={`flex justify-center items-center w-1/3 min-h-16`} onClick={() => router.push(`/${type}`)}>
       {children}
     </button>
   );
