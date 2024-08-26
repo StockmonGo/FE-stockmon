8e76e6bf75c8080e21682b4e0a84d4d836f0da84
KimRiun <kimriun27@gmail.com>
2024-08-26 13:14:52 +0900

    feat: 검색바 ui 생성

---
+++ src/components/ui/SearchBar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BsSearch } from "react-icons/bs";
+
+type Props = {
+  placeholder: string;
+};
+
+export default function SearchBar({ placeholder }: Props) {
+  return (
+    <div className="w-full p-3 flex justify-center items-center rounded-lg bg-white">
+      <BsSearch className="w-6 h-6 m-1 text-stock-dark-300" />
+      <input
+        className="w-full px-2 font-ptr"
+        type="text"
+        placeholder={placeholder}
+      />
+    </div>
+  );
+}
