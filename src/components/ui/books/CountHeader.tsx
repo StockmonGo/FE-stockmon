5a0661cef379d89df27ef517a6d55d95a680fe6f
KimRiun <kimriun27@gmail.com>
2024-08-26 15:55:22 +0900

    design: 도감에서 포획한 수 색을 회색으로 수정

--- src/components/ui/books/CountHeader.tsx
+++ src/components/ui/books/CountHeader.tsx
@@ -10,13 +10,13 @@ export default function CountHeader({ isLoading, count }: Props) {
   const STOCKMON_MAX = 200;
 
   return (
-    <div className="w-full h-full flex justify-center items-center font-ptb text-2xl">
+    <div className="w-full h-full p-3 flex justify-center items-center ">
       {isLoading ? (
         <Skeleton className="w-1/4 h-8 rounded-lg" />
       ) : (
-        <>
+        <p className=" font-bold text-2xl text-stock-dark-600">
           {count}/{STOCKMON_MAX}
-        </>
+        </p>
       )}
     </div>
   );
