eb5e89207fa2254ea971ad7dfc7095b9a9bc5d30
hyuna <kook980316@gmail.com>
2024-08-28 16:30:33 +0900

    feat: 회원가입, 로그인 페이지로 이동하는 접근 페이지 추가

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,16 +1,17 @@
+"use client";
+import Button from "@/components/ui/Button";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function Account() {
+  const router = useRouter();
   return (
-    <div>
-      <ol>
-        <li>
-          <a href="/account/login">login</a>
-        </li>
-        <li>
-          <a href="/account/register">register</a>
-        </li>
-      </ol>
+    <div className="grid justify-items-center space-y-12">
+      <img src="/images/logo-280x280.png" alt="로고" />
+      <div className="w-32 grid">
+        <Button text="회원가입" onClick={() => router.push("/account/register")} />
+        <Button text="로그인" onClick={() => router.push("/account/login")} />
+      </div>
     </div>
   );
 }
