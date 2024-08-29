70a5bed053ac31d1d95228f982767d481170f192
KimRiun <kimriun27@gmail.com>
2024-08-29 16:18:15 +0900

    design: 스톡몬 상세 페이지에서 스톡몬 클릭시 애니메이션 추가

--- src/components/ui/books/StockmonCard.tsx
+++ src/components/ui/books/StockmonCard.tsx
@@ -1,6 +1,8 @@
+"use client";
 import { StockmonDetailType } from "@/types/stockmons";
 import { useParams, useRouter } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "@/app/animations.css";
 
 type Props = {
   data: StockmonDetailType;
@@ -9,15 +11,30 @@ type Props = {
 export default function StockmonCard({ data }: Props) {
   const params = useParams();
   const router = useRouter();
+  const [animate, setAnimate] = useState(false);
 
+  const handleClick = () => {
+    setAnimate(true); // 애니메이션을 시작
+  };
+
+  useEffect(() => {
+    if (animate) {
+      const timeout = setTimeout(() => {
+        setAnimate(false); // 일정 시간 후 애니메이션 제거
+      }, 500); // 애니메이션의 지속 시간에 맞게 조정
+
+      return () => clearTimeout(timeout); // 클린업
+    }
+  }, [animate]);
   return (
     <article className="w-full p-5 font-ptb text-lg bg-border-custom-dotted bg-stock-blue-200">
       <figure className="aspect-square flex flex-col justify-between gap- p-5 bg-[url('/images/bgStamp.svg')] bg-no-repeat bg-cover">
         <p className="text-stock-dark-300">No.{data.stockCode}</p>
         <img
-          className="w-2/3 mx-auto"
+          className={`w-2/3 mx-auto ${animate ? "gelatine" : ""}`}
           src={data.imageUrl}
           alt={data.stockmonName}
+          onClick={handleClick}
         />
         <div className="text-right">
           <button
