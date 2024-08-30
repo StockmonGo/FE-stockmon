18b587d12d6e0d13968a222e2507a1b7e0bd4983
KimRiun <kimriun27@gmail.com>
2024-08-30 14:46:02 +0900

    feat: 차트 바탕색 및 테두리 설정

--- src/components/ui/books/StockmonChart.tsx
+++ src/components/ui/books/StockmonChart.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function StockmonChart() {
-  return <div>StockmonChart</div>;
+  return (
+    <article className="w-full p-4 bg-white border-4 border-dashed border-[#98DFAF]">
+      StockmonChart
+    </article>
+  );
 }
