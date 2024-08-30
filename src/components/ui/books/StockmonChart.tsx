5d9e014d3f0684f0afa85e896d33237598cd20e2
KimRiun <kimriun27@gmail.com>
2024-08-30 15:01:35 +0900

    feat: 차트가 그려질 공간 지정

--- src/components/ui/books/StockmonChart.tsx
+++ src/components/ui/books/StockmonChart.tsx
@@ -2,8 +2,18 @@ import React from "react";
 
 export default function StockmonChart() {
   return (
-    <article className="w-full p-4 bg-white border-4 border-dashed border-[#98DFAF]">
-      StockmonChart
+    <article className="relative w-full p-4 bg-white border-4 border-dashed border-[#98DFAF]">
+      <img
+        className="absolute -left-8 -top-2 scale-[1.4] opacity-80"
+        src="/images/chartWave.svg"
+        alt="물결 이미지"
+      />
+      <div className="absolute py-2 px-8 -top-2 -right-2 rotate-3 font-ptb text-lg text-stock-dark-400 border-2 border-[#98DFAF] bg-[#ECFFEE]">
+        월별 차트
+      </div>
+      <div className="w-full h-60 text-center bg-stock-dark-400 mt-6">
+        차트 그리기
+      </div>
     </article>
   );
 }
