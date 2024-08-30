5443d3d757674c963553ad9ebdd43a21ec600440
hyuna <kook980316@gmail.com>
2024-08-30 09:28:12 +0900

    feat: 스톡몬들의 위치 랜덤으로 배치

--- src/app/yard/page.tsx
+++ src/app/yard/page.tsx
@@ -1,4 +1,5 @@
 import BtnClose from "@/components/ui/BtnClose";
+import { randomInt } from "crypto";
 
 export default function Yard() {
   // 스톡목 더미
@@ -22,8 +23,17 @@ export default function Yard() {
       isGood: true,
     },
   ];
+
   return (
     <div className="w-screen max-w-lg h-screen bg-[url('/images/yardBg.png')] bg-cover bg-no-repeat relative">
+      {stockmons.map((item) => {
+        const top = randomInt(500) + "px";
+        const left = randomInt(300) + "px";
+
+        return (
+          <img src={item.imgUrl} alt={item.name} key={item.id} className="absolute w-24 h-24" style={{ top, left }} />
+        );
+      })}
       <div className="fixed bottom-6 w-full flex justify-center">
         <BtnClose />
       </div>
