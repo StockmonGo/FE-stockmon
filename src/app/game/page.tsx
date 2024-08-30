59054cc0869d2a6cf1be97ca06b99da80b4e05fe
soyeon <uyyu1734@gmail.com>
2024-08-30 11:43:22 +0900

    feat: 스톡몬 획득 페이지 추가

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,24 +1,24 @@
 "use client";
+import NewStockmon from "@/components/game/NewStockmon";
 import TimingGame from "@/components/game/TimingGame";
+import PeachCount from "@/components/ui/game/PeachCount";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
+type GAME_STATUS = "playing" | "done";
 export default function Game() {
+  const [gameStatus, setGameStatus] = useState<GAME_STATUS>("playing");
+  // TODO: 선택한 스톡몬 id 가져와서 노출 catchStockmon에 api붙히기
+  // TODO: 잡은 스톡몬 정보 NewStockmon에 뿌려주기
+
+  const catchStockmon = () => {
+    setGameStatus("done");
+  };
   return (
     <div className="h-full">
-      <div className="fixed flex items-end min-w-28 justify-end right-[20px]">
-        <Image
-          alt="peach"
-          src="/images/peach.svg"
-          width={44}
-          height={44}
-          className="absolute right-[60px]"
-        />
-        <div className="bg-white rounded-lg px-2 py-1 h-fit mb-[2px]">
-          <p className="font-ptr text-stock-dark-800 leading-5"> 18 / 20</p>
-        </div>
-      </div>
-      <TimingGame />
+      <PeachCount />
+      {<TimingGame catchStockmon={catchStockmon} />}
+      {gameStatus === "done" && <NewStockmon />}
     </div>
   );
 }
