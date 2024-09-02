4fcf8d00e3ee0798f9cd5d74f9e2486bb27a2dac
KimRiun <kimriun27@gmail.com>
2024-09-02 09:45:21 +0900

    feat: 주식 받기 버튼 누를 시 모달 흐름 처리

--- src/app/books/[id]/collection/page.tsx
+++ src/app/books/[id]/collection/page.tsx
@@ -1,36 +1,77 @@
 "use client";
 import { useParams } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Error from "@/components/ui/Error";
 import Loading from "@/components/ui/Loading";
 import useSWR from "swr";
 import { COLLECTION_MAX } from "@/types/stockmons";
 import Button from "@/components/ui/Button";
 import data from "@/../dummy/books/bookDetail.json";
+import Modal from "@/components/ui/Modal";
 
 const fetcher = (url: string) => {
   //TODO: 스톡몬 개별 페이지 조회 api 연결
   return data;
 };
 
+const fetcher2 = (url: string): Promise<ProfileType> => {
+  // TODO: 스톡몬 개별 페이지 조회 api 연결
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        hasAccount: false,
+      });
+    }, 1000);
+  });
+};
+
+type ProfileType = {
+  hasAccount: boolean;
+};
+
 export default function Collection() {
   const params = useParams();
-  const { data, error } = useSWR(`/api/stockmons/${params.id}`, fetcher);
+  const { data: stockmonData, error: stockmonError } = useSWR(
+    `/api/stockmons/${params.id}`,
+    fetcher
+  );
+  const [accountModalOpen, setAccountModalOpen] = useState(false);
+  const [getStockModalOpen, setGetStockModalOpen] = useState(false);
+  const [buttonLoading, setButtonLoading] = useState(false);
 
-  if (error) return <Error />;
-  if (!data) {
+  if (stockmonError) return <Error />;
+  if (!stockmonData) {
     return <Loading />;
   }
 
-  const handleExchange = () => {
-    console.log("주식 받기");
+  const handleExchange = async () => {
+    setButtonLoading(true);
+    const profileData: ProfileType = await fetcher2(`/api/core/users/account`);
+    setButtonLoading(false);
+    if (profileData.hasAccount) {
+      setGetStockModalOpen(true);
+      handleCloseGetStockModal();
+    } else {
+      setAccountModalOpen(true);
+    }
+  };
+
+  const handleConfirmAccountModal = () => {
+    setAccountModalOpen(false);
+    setGetStockModalOpen(true);
+  };
+
+  const handleCloseGetStockModal = async () => {
+    // TODO: 주식 받기 api 요청
+    setGetStockModalOpen(false);
+    console.log(stockmonData.stockCode + " 받기 완료");
   };
 
   return (
-    <div className="flex flex-col gap-8 justify-center items-center">
+    <div className="flex flex-col gap-8 overflow-scroll pb-20 justify-center items-center">
       <header className="flex flex-col gap-2 justify-center items-center">
         <div className="py-2 px-3 text-2xl font-bold text-stock-dark-500 border-2 border-white rounded-lg bg-somsatang-gradient">
-          {data.catchCount} / {COLLECTION_MAX}
+          {stockmonData.catchCount} / {COLLECTION_MAX}
         </div>
         <p className="font-ptr text-stock-dark-400">
           스톡몬을 모아 주식으로 받을 수 있어요.
@@ -42,11 +83,11 @@ export default function Collection() {
           style={{ minHeight: "calc(100vh / 2)" }}
         >
           {Array.from({ length: COLLECTION_MAX }).map((_, index) =>
-            index < data.catchCount ? (
+            index < stockmonData.catchCount ? (
               <img
                 key={index}
-                src={data.imageUrl}
-                alt={data.stockmonName}
+                src={stockmonData.imageUrl}
+                alt={stockmonData.stockmonName}
                 className="aspect-square object-cover bg-white rounded-lg"
               />
             ) : (
@@ -58,12 +99,32 @@ export default function Collection() {
           )}
         </div>
       </main>
-      <footer className="w-32 justify-self-center mt-4">
+      <footer className="w-32 mt-4 justify-self-center">
         <Button
           onClick={handleExchange}
           text="주식 받기"
-          disabled={data.catchCount < COLLECTION_MAX}
+          disabled={stockmonData.catchCount < COLLECTION_MAX}
+          isLoading={buttonLoading}
         />
+        <Modal
+          open={accountModalOpen}
+          describe={`계좌를 만들고 '${stockmonData.stockmonName}'을 '${stockmonData.stockName}'(으)로 받으시겠습니까?`}
+          onClose={() => setAccountModalOpen(false)}
+          onConfirm={handleConfirmAccountModal}
+          hasClose
+        ></Modal>
+        <Modal
+          open={getStockModalOpen}
+          title={`대 ${stockmonData.stockmonName}`}
+          describe='"그동안 우리 아이들을 잘 돌봐주셔서 감사합니다. 이 은혜는 돈으로 갚겠습니다."'
+          onClose={handleCloseGetStockModal}
+        >
+          <img
+            src={stockmonData.imageUrl}
+            alt={stockmonData.stockmonName}
+            className="min-w-44 aspect-square object-cover rounded-lg"
+          />
+        </Modal>
       </footer>
     </div>
   );
