a234def9d76ba1afd62d13be066b7064b7f9c7f4
hyuna <kook980316@gmail.com>
2024-09-02 15:17:09 +0900

    chore: 맵 관련 스톡몬, 스톡타워 타입 추가

---
+++ src/types/location.ts
@@ -0,0 +1,13 @@
+export type StockmonLocation = {
+  id: number;
+  stockmon_id: number;
+  latitude: number;
+  longitude: number;
+};
+
+export type StockTowerLocation = {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+};
