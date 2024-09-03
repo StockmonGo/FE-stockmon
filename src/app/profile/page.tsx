e6aef27bcf870aa6f0ec093bbbf58c6e96a1b9cd
hyuna <kook980316@gmail.com>
2024-09-03 13:27:09 +0900

    refactor: SWR을 사용하기 위해 함수를 hook으로 분리

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,39 +1,31 @@
 "use client";
 import memberAPI from "@/apis/memberAPI";
 import CommonLayout from "@/components/ui/CommonLayout";
+import Loading from "@/components/ui/Loading";
 import UserMenu from "@/components/ui/profile/UserMenu";
 import UserProfile from "@/components/ui/profile/UserProfile";
+import { getMemberProfile } from "@/hooks/useMember";
 import { IMember } from "@/types/member";
-import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 export default function Profile() {
-  // TODO 사용자 정보랑 연결
-  const [user, setUser] = useState<IMember>({
-    nickname: "",
-    hasAccount: false,
-    accountNumber: "",
-  });
-  const service = new memberAPI();
+  const { data: member, error } = useSWR<IMember | undefined>("member profile", getMemberProfile);
 
-  useEffect(() => {
-    service
-      .getMemberProfile()
-      .then((res) => {
-        console.log(res);
-        if (res.member) {
-          setUser(res.member);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  // 에러 처리
+  if (error) {
+    console.error(error);
+    return <div>Failed to load user profile.</div>;
+  }
+
+  if (!member) {
+    return <Loading />;
+  }
 
   return (
     <CommonLayout title="마이페이지">
       <div className="w-full h-full flex flex-col items-center gap-6">
-        <UserProfile nickname={user.nickname} />
-        <UserMenu accountNumber={user.accountNumber ? user.accountNumber : ""} />
+        <UserProfile nickname={member.nickname} />
+        <UserMenu accountNumber={member.accountNumber ? member.accountNumber : ""} />
       </div>
     </CommonLayout>
   );
