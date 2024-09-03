f56b6eb971c5274b046f53ab46d65321bde8a0d6
hyuna <kook980316@gmail.com>
2024-09-03 10:55:47 +0900

    feat: 사용자 정보 연결

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,21 +3,27 @@ import memberAPI from "@/apis/memberAPI";
 import CommonLayout from "@/components/ui/CommonLayout";
 import UserMenu from "@/components/ui/profile/UserMenu";
 import UserProfile from "@/components/ui/profile/UserProfile";
+import { IMember } from "@/types/member";
 import { useEffect, useState } from "react";
 
 export default function Profile() {
   // TODO 사용자 정보랑 연결
-  const [user, setUser] = useState({
-    nickname: "희을을",
-    hasAccount: true,
-    accountNumber: "010-12345-67890",
+  const [user, setUser] = useState<IMember>({
+    nickname: "",
+    hasAccount: false,
+    accountNumber: "",
   });
   const service = new memberAPI();
 
   useEffect(() => {
     service
       .getMemberProfile()
-      .then((res) => console.log(res))
+      .then((res) => {
+        console.log(res);
+        if (res.member) {
+          setUser(res.member);
+        }
+      })
       .catch((err) => {
         console.log(err);
       });
