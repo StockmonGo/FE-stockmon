bbc2932df6cd8e45257a0827da8634431a8317bf
hyuna <kook980316@gmail.com>
2024-09-03 11:31:10 +0900

    feat: 계좌 정보 불러오는 API 추가

--- src/apis/memberAPI.ts
+++ src/apis/memberAPI.ts
@@ -38,4 +38,40 @@ export default class memberAPI extends BaseApi {
       }
     }
   }
+
+  async getAccountStatus(): Promise<{
+    status: number;
+    message: string;
+    hasAccount: boolean | null;
+  }> {
+    try {
+      const resp = await this.fetcher.get("/api/core/users/account");
+
+      if (resp.status === 200 && resp.data.data) {
+        // 주변 조회 성공
+        return {
+          status: 200,
+          message: "계좌 정보 불러오기 성공",
+          hasAccount: resp.data.data.hasAccount,
+        };
+      }
+      throw Error("실패");
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error(error.response?.data);
+        return {
+          status: 400,
+          message: error.response?.data.message || "알 수 없는 오류 발생",
+          hasAccount: null,
+        };
+      } else {
+        console.error(error);
+        return {
+          status: 500,
+          message: "서버 오류 발생",
+          hasAccount: null,
+        };
+      }
+    }
+  }
 }
