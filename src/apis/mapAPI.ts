853b3414fff0be54d5ae610c5fff25e7debdaf5d
hyuna <kook980316@gmail.com>
2024-09-03 09:19:24 +0900

    fix: 맵 데이터 타입 변경

--- src/apis/mapAPI.ts
+++ src/apis/mapAPI.ts
@@ -10,25 +10,25 @@ export default class mapAPI extends BaseApi {
   async getMapInfo(location: ILocation): Promise<{
     status: number;
     message: string;
-    data: { stockmon: Array<StockmonLocation>; stockTowers: Array<StockTowerLocation> } | null;
+    mapInfo: { stockmon: Array<StockmonLocation>; stockTowers: Array<StockTowerLocation> } | null;
   }> {
     try {
       const resp = await this.fetcher.post("/api/core/maps/stockmons", {
         ...location,
       });
 
-      if (resp.status === 200 && resp.data.user) {
+      if (resp.status === 200 && resp.data) {
         // 주변 조회 성공
         return {
           status: 200,
           message: "지도 정보 불러오기 성공",
-          data: resp.data,
+          mapInfo: resp.data.data,
         };
       }
       throw Error("실패");
     } catch (error) {
       console.error(error);
-      return { status: 400, message: "사용자 좌표 오류", data: null };
+      return { status: 400, message: "사용자 좌표 오류", mapInfo: null };
     }
   }
 }
