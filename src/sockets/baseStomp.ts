ebfa8341d158d1b738063d4d33268252945429fc
KimRiun <kimriun27@gmail.com>
2024-09-03 13:26:54 +0900

    feat: 소켓 클라이언트 생성

---
+++ src/sockets/baseStomp.ts
@@ -0,0 +1,18 @@
+import * as StompJs from "@stomp/stompjs";
+
+export const client = new StompJs.Client({
+  brokerURL: "ws://api.stockmon.world/api/stock/gs-guide-websocket",
+  // debug: function (str) {
+  //   console.log(str);
+  // },
+  reconnectDelay: 5000,
+  heartbeatIncoming: 4000,
+  heartbeatOutgoing: 4000,
+});
+
+client.onStompError = function (frame) {
+  console.log("Broker reported error: " + frame.headers["message"]);
+  console.log("Additional details: " + frame.body);
+};
+
+// client.activate();
