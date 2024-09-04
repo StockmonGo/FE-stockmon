2acbce90d82cbf972ca876ad3e423c93e0ef0c4a
KimRiun <kimriun27@gmail.com>
2024-09-04 10:58:05 +0900

    feat: api 응답 처리시 반복되는 오류 처리 부분 함수화로 중복 제거

--- src/apis/baseAPI.ts
+++ src/apis/baseAPI.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { HttpStatusCode } from "axios";
 const { NEXT_PUBLIC_BASE_URL } = process.env;
 console.log("url:", process.env.NEXT_PUBLIC_BASE_URL);
 //우리 서버랑 통신할 Api 세팅
@@ -14,5 +14,20 @@ export class BaseApi {
         Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
       },
     });
+  }
+}
+
+// 공통 에러 처리
+export function handleApiError(error: any): never {
+  if (axios.isAxiosError(error)) {
+    if (error.response?.data.status === HttpStatusCode.BadRequest) {
+      throw new Error("로그인이 만료되었습니다.");
+    }
+
+    console.error("Axios 에러:", error.response?.data.message || error.message);
+    throw new Error("Axios 에러가 발생하였습니다.");
+  } else {
+    console.error("알 수 없는 에러:", error);
+    throw new Error("알 수 없는 에러가 발생하였습니다.");
   }
 }
