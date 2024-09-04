fb59655d1b01a4d787ab0cc1378e5f695794106a
hyuna <kook980316@gmail.com>
2024-09-04 17:57:46 +0900

    wip: buffer  영역 저장해두고 계산하는 로직 추가 중

--- src/app/world/page.tsx
+++ src/app/world/page.tsx
@@ -37,111 +37,129 @@ export default function World() {
     kakaoMapScript.async = false;
     kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_JS_KEY}&autoload=false`;
     document.head.appendChild(kakaoMapScript);
-
     const onLoadKakaoAPI = () => {
       window.kakao.maps.load(() => {
+        const container = document.getElementById("map");
+        let map: any;
+        let userMarker: any;
+
         if (navigator.geolocation) {
-          // navigator.geolocation.watchPosition(function (position) {
-          // TODO 위치가 변경됐을 때 요청을 다시 보내는 부분
-          navigator.geolocation.getCurrentPosition(function (position) {
+          navigator.geolocation.watchPosition(function (position) {
+            // 사용자 위치
             const latitude = position.coords.latitude; // 위도
             const longitude = position.coords.longitude; // 경도
             const myLocation = new window.kakao.maps.LatLng(latitude, longitude);
-            // 성수로 일단 고정
-            // const myLocation = new window.kakao.maps.LatLng(37.5451626, 127.0568792);
-            console.log(myLocation);
+            console.log("사용자 위치 ", myLocation);
 
-            const container = document.getElementById("map");
-            const options = {
-              center: myLocation,
-              level: 2,
-              draggable: false,
-            };
-            console.log(myLocation);
-
-            const map = new window.kakao.maps.Map(container, options);
-            const bounds = map.getBounds();
-            console.log(bounds.toString());
-
-            const userImageSrc = "images/tempPerson.svg";
-            const userImageSize = new window.kakao.maps.Size(100, 100);
-            const imageOption = { offset: new window.kakao.maps.Point(27, 69) };
-            const markerImage = new window.kakao.maps.MarkerImage(userImageSrc, userImageSize, imageOption);
+            // 지도를 처음 한 번만 생성
+            if (!map) {
+              const options = {
+                center: myLocation,
+                level: 2,
+                draggable: false,
+              };
+              map = new window.kakao.maps.Map(container, options);
+            } else {
+              map.setCenter(myLocation);
+            }
 
             // 사용자 마커
-            const userMarker = new window.kakao.maps.Marker({
-              position: myLocation,
-              image: markerImage,
-            });
-            userMarker.setMap(map);
+            if (!userMarker) {
+              const userImageSrc = "images/tempPerson.svg";
+              const userImageSize = new window.kakao.maps.Size(100, 100);
+              const imageOption = { offset: new window.kakao.maps.Point(27, 69) };
+              const markerImage = new window.kakao.maps.MarkerImage(userImageSrc, userImageSize, imageOption);
 
-            // 주변 스톡타워, 스톡몬 정보
-            service.getMapInfo({ latitude, longitude }).then((res) => {
-              console.log(res);
-              // 스톡타워 마커
-              const stockTowerPositions = res?.stockTowers.map((tower) => ({
-                id: tower.id,
-                title: tower.name,
-                latlng: new window.kakao.maps.LatLng(tower.latitude, tower.longitude),
-              }));
-              const towerImageSrc = "images/peachTower.svg";
+              userMarker = new window.kakao.maps.Marker({
+                position: myLocation,
+                image: markerImage,
+                map: map,
+              });
+            } else {
+              userMarker.setPosition(myLocation);
+            }
 
-              if (stockTowerPositions) {
-                for (let i = 0; i < stockTowerPositions.length; i++) {
-                  const towerImageSize = new window.kakao.maps.Size(80, 80);
-                  const towerImage = new window.kakao.maps.MarkerImage(towerImageSrc, towerImageSize);
-                  const stockTower = new window.kakao.maps.Marker({
-                    map: map,
-                    position: stockTowerPositions[i].latlng,
-                    title: stockTowerPositions[i].title,
-                    image: towerImage,
-                  });
+            // 버퍼 없거나 버퍼를 벗어나면 버퍼 계산 후 주변 정보 불러오기
+            if (
+              buffer.maxLat == 0 ||
+              latitude <= buffer.minLat ||
+              latitude >= buffer.maxLat ||
+              longitude <= buffer.minLon ||
+              longitude >= buffer.maxLon
+            ) {
+              const maxLat = latitude + 0.0048;
+              const minLat = latitude - 0.0048;
+              const maxLon = longitude + 0.003;
+              const minLon = longitude - 0.003;
+              setBuffer({ maxLat, minLat, maxLon, minLon });
+              console.log(buffer);
+              // 주변 스톡타워, 스톡몬 정보
+              service.getMapInfo({ latitude, longitude }).then((res) => {
+                console.log(res);
+                // 스톡타워 마커
+                const stockTowerPositions = res?.stockTowers.map((tower) => ({
+                  id: tower.id,
+                  title: tower.name,
+                  latlng: new window.kakao.maps.LatLng(tower.latitude, tower.longitude),
+                }));
+                const towerImageSrc = "images/peachTower.svg";
 
-                  window.kakao.maps.event.addListener(stockTower, "click", () => {
-                    checkStockTower(stockTowerPositions[i].id);
-                    setTowerName(stockTowerPositions[i].title);
-                  });
+                if (stockTowerPositions) {
+                  for (let i = 0; i < stockTowerPositions.length; i++) {
+                    const towerImageSize = new window.kakao.maps.Size(80, 80);
+                    const towerImage = new window.kakao.maps.MarkerImage(towerImageSrc, towerImageSize);
+                    const stockTower = new window.kakao.maps.Marker({
+                      map: map,
+                      position: stockTowerPositions[i].latlng,
+                      title: stockTowerPositions[i].title,
+                      image: towerImage,
+                    });
 
-                  stockTower.setMap(map);
+                    window.kakao.maps.event.addListener(stockTower, "click", () => {
+                      checkStockTower(stockTowerPositions[i].id);
+                      setTowerName(stockTowerPositions[i].title);
+                    });
+
+                    stockTower.setMap(map);
+                  }
                 }
-              }
 
-              // 스톡몬 마커
-              const stockmonPositions = res?.stockmons.map((stockmon) => ({
-                id: stockmon.id,
-                latlng: new window.kakao.maps.LatLng(stockmon.latitude, stockmon.longitude),
-              }));
+                // 스톡몬 마커
+                const stockmonPositions = res?.stockmons.map((stockmon) => ({
+                  id: stockmon.id,
+                  latlng: new window.kakao.maps.LatLng(stockmon.latitude, stockmon.longitude),
+                }));
 
-              if (stockmonPositions) {
-                for (let i = 0; i < stockmonPositions.length; i++) {
-                  const stockmonImaegSize = new window.kakao.maps.Size(100, 100);
-                  const stockmonImgSrc = `${process.env.NEXT_PUBLIC_S3_URL}/${i}.png`;
-                  const stockmonImage = new window.kakao.maps.MarkerImage(stockmonImgSrc, stockmonImaegSize);
-                  const stockmon = new window.kakao.maps.Marker({
-                    map: map,
-                    position: stockmonPositions[i].latlng,
-                    id: stockmonPositions[i].id,
-                    image: stockmonImage,
-                  });
+                if (stockmonPositions) {
+                  for (let i = 0; i < stockmonPositions.length; i++) {
+                    const stockmonImaegSize = new window.kakao.maps.Size(100, 100);
+                    const stockmonImgSrc = `${process.env.NEXT_PUBLIC_S3_URL}/${i}.png`;
+                    const stockmonImage = new window.kakao.maps.MarkerImage(stockmonImgSrc, stockmonImaegSize);
+                    const stockmon = new window.kakao.maps.Marker({
+                      map: map,
+                      position: stockmonPositions[i].latlng,
+                      id: stockmonPositions[i].id,
+                      image: stockmonImage,
+                    });
 
-                  window.kakao.maps.event.addListener(stockmon, "click", () => {
-                    setStockmonId(stockmonPositions[i].id);
-                  });
+                    window.kakao.maps.event.addListener(stockmon, "click", () => {
+                      setStockmonId(stockmonPositions[i].id);
+                    });
 
-                  stockmon.setMap(map);
+                    stockmon.setMap(map);
+                  }
                 }
-              }
-            });
+              });
+            }
           });
         } else {
-          const container = document.getElementById("map");
+          // Geolocation API 사용 불가 시 기본 위치 설정
           const options = {
             center: new window.kakao.maps.LatLng(33.450701, 126.570667),
             level: 2,
             draggable: false,
           };
-
-          const map = new window.kakao.maps.Map(container, options);
+          map = new window.kakao.maps.Map(container, options);
         }
       });
     };
--- src/store/store.ts
+++ src/store/store.ts
@@ -5,6 +5,7 @@ export const userAtom = atom({ nickname: "", jwt: "" });
 // Create your atoms and derivatives
 export const textAtom = atom("hello");
 const uppercaseAtom = atom((get) => get(textAtom).toUpperCase());
+export const buffetAtom = atom({ maxLat: 0, minLat: 0, maxLon: 0, minLon: 0 });
 
 // Set the string key and the initial value
 export const userLocalAtom = atomWithStorage("user", "");
