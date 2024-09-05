fc0a7e8d96b80d423e1c7927577e2dd59e6c17c1
soyeon <uyyu1734@gmail.com>
2024-09-05 13:54:41 +0900

    fix: 루트페이지에서 라우팅 useEffect로 감싸기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,20 @@
 import { accessTokenAtom } from "@/store/store";
 import { useAtom } from "jotai";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
-  const [accessToken, setAccessToken] = useAtom(accessTokenAtom);
-  if (accessToken) {
-    router.replace("/world");
-  } else {
-    router.replace("/users/register");
-  }
+  const [accessToken] = useAtom(accessTokenAtom);
+
+  useEffect(() => {
+    // accessToken의 상태에 따라 페이지를 리다이렉트
+    if (accessToken) {
+      router.replace("/world");
+    } else {
+      router.replace("/users/register");
+    }
+  }, [accessToken, router]);
+
+  return null; // 혹은 로딩 상태를 표시하는 JSX를 반환
 }
