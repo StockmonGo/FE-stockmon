fda7ecb499c773b9971a2bf9980e18afd08e75db
KimRiun <kimriun27@gmail.com>
2024-09-06 08:44:42 +0900

    fix: 에러 페이지 이미지 수정

--- src/app/[...404]/page.tsx
+++ src/app/[...404]/page.tsx
@@ -2,5 +2,18 @@ import Error from "@/components/ui/Error";
 import React from "react";
 
 export default function NotFound() {
-  return <Error message="존재하지 않는 페이지 입니다." />;
+  return (
+    <div className="w-full h-full overflow-x-hidden overflow-y-scroll">
+      <div className="fixed w-full h-full overflow-hidden z-0">
+        <div
+          className="bg-cover bg-center w-full h-full fixed z-[-1]"
+          style={{ backgroundImage: "url('/images/bg.jpg')" }}
+        ></div>
+        <Error
+          message="존재하지 않는 페이지 입니다."
+          imgUrl={`${process.env.NEXT_PUBLIC_S3_URL}/${13}.png`}
+        />
+      </div>
+    </div>
+  );
 }
