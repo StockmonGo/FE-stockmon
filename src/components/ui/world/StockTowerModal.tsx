acdb83c13fa2dc53cbf0e1c542c611f7fce44cdb
soyeon <uyyu1734@gmail.com>
2024-09-05 16:18:33 +0900

    design: world pc버전에서 가운데 정렬

--- src/components/ui/world/StockTowerModal.tsx
+++ src/components/ui/world/StockTowerModal.tsx
@@ -9,7 +9,12 @@ type Props = {
   towerActive: boolean;
   service: mapAPI;
 };
-export default function StockTowerModal({ name, towerId, towerActive, service }: Props) {
+export default function StockTowerModal({
+  name,
+  towerId,
+  towerActive,
+  service,
+}: Props) {
   const [stockBallNum, setStockBallNum] = useState(0);
   const [showPeach, setShowPeach] = useState(false);
   const [flipCss, setFlipCss] = useState("");
@@ -46,25 +51,47 @@ export default function StockTowerModal({ name, towerId, towerActive, service }:
   return (
     <>
       {showPeach ? (
-        <p className="fade-in-down text-white text-4xl fixed top-4 z-20">+ {stockBallNum}</p>
+        <p className="fade-in-down text-white text-4xl fixed top-4 z-20">
+          + {stockBallNum}
+        </p>
       ) : (
         <p className="text-white text-4xl fixed top-4 z-20">{name}</p>
       )}
-      <div
-        className={`${firstCss} z-10 ${
-          active ? "border-stock-red" : "border-stock-dark-500"
-        } border-[100px] w-screen min-w-[450px] scale-x-[1.7] scale-y-110 rounded-full h-full fixed top-0 transform -translate-x-1/2 flex justify-center items-center`}
-      >
-        {active ? (
-          <img src="/images/peachTower.svg" alt="피치타워" className={`w-3/4 ${flipCss}`} onClick={handleTowerClick} />
-        ) : (
-          <img src="/images/peachTowerGrey.svg" alt="피치타워" className={`w-3/4`} />
-        )}
+      <div className="overflow-hidden fixed max-w-xl w-full h-screen">
+        <div
+          className={`${firstCss} z-10 ${
+            active ? "border-stock-red" : "border-stock-dark-500"
+          } absolute max-w-[654px] border-[100px] w-screen min-w-[450px] scale-x-[1.7] scale-y-110 rounded-full h-full fixed top-0 transform -translate-x-1/2 flex justify-center items-center`}
+        >
+          {active ? (
+            <img
+              src="/images/peachTower.svg"
+              alt="피치타워"
+              className={`w-3/4 ${flipCss}`}
+              onClick={handleTowerClick}
+            />
+          ) : (
+            <img
+              src="/images/peachTowerGrey.svg"
+              alt="피치타워"
+              className={`w-3/4`}
+            />
+          )}
+        </div>
       </div>
+
       {showPeach && (
         <>
-          <img src="/images/peach.svg" alt="복숭아" className="bounce w-14 h-14 z-20 fixed bottom-4 right-20" />
-          <img src="/images/peach.svg" alt="복숭아" className="bounce2 w-14 h-14 z-20 fixed bottom-4 left-24" />
+          <img
+            src="/images/peach.svg"
+            alt="복숭아"
+            className="bounce w-14 h-14 z-20 fixed bottom-4 right-20"
+          />
+          <img
+            src="/images/peach.svg"
+            alt="복숭아"
+            className="bounce2 w-14 h-14 z-20 fixed bottom-4 left-24"
+          />
         </>
       )}
     </>
