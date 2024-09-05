d9c97203fc59b7182ff155d10e3906bbe3cbd7f4
hyuna <kook980316@gmail.com>
2024-09-05 10:24:18 +0900

    feat: 버퍼 계산해서 범위 바깥일 때만 요청하도록 변경

--- src/app/world/page.tsx
+++ src/app/world/page.tsx
@@ -53,128 +53,145 @@ export default function World() {
         const container = document.getElementById("map");
         let map: any;
         let userMarker: any;
+        let buffer: { maxLat: number; minLat: number; maxLon: number; minLon: number };
 
-        if (navigator.geolocation) {
-          navigator.geolocation.watchPosition(function (position) {
-            // 사용자 위치
-            const latitude = position.coords.latitude; // 위도
-            const longitude = position.coords.longitude; // 경도
-            const myLocation = new window.kakao.maps.LatLng(latitude, longitude);
-            console.log("사용자 위치 ", myLocation);
-
-            // 지도를 처음 한 번만 생성
-            if (!map) {
-              const options = {
-                center: myLocation,
-                level: 2,
-                draggable: false,
-              };
-              map = new window.kakao.maps.Map(container, options);
-            } else {
-              map.setCenter(myLocation);
-            }
-
-            // 사용자 마커
-            if (!userMarker) {
-              const userImageSrc = "images/tempPerson.svg";
-              const userImageSize = new window.kakao.maps.Size(100, 100);
-              const imageOption = { offset: new window.kakao.maps.Point(27, 69) };
-              const markerImage = new window.kakao.maps.MarkerImage(userImageSrc, userImageSize, imageOption);
-
-              userMarker = new window.kakao.maps.Marker({
-                position: myLocation,
-                image: markerImage,
-                map: map,
-              });
-            } else {
-              userMarker.setPosition(myLocation);
-            }
-
-            // 버퍼 없거나 버퍼를 벗어나면 버퍼 계산 후 주변 정보 불러오기
-            if (
-              buffer.maxLat == 0 ||
-              latitude <= buffer.minLat ||
-              latitude >= buffer.maxLat ||
-              longitude <= buffer.minLon ||
-              longitude >= buffer.maxLon
-            ) {
-              const maxLat = latitude + 0.0048;
-              const minLat = latitude - 0.0048;
-              const maxLon = longitude + 0.003;
-              const minLon = longitude - 0.003;
-              setBuffer({ maxLat, minLat, maxLon, minLon });
-              console.log(buffer);
-              // 주변 스톡타워, 스톡몬 정보
-              service.getMapInfo({ latitude, longitude }).then((res) => {
-                console.log(res);
-                // 스톡타워 마커
-                const stockTowerPositions = res?.stockTowers.map((tower) => ({
-                  id: tower.id,
-                  title: tower.name,
-                  latlng: new window.kakao.maps.LatLng(tower.latitude, tower.longitude),
-                }));
-                const towerImageSrc = "images/peachTower.svg";
-
-                if (stockTowerPositions) {
-                  for (let i = 0; i < stockTowerPositions.length; i++) {
-                    const towerImageSize = new window.kakao.maps.Size(80, 80);
-                    const towerImage = new window.kakao.maps.MarkerImage(towerImageSrc, towerImageSize);
-                    const stockTower = new window.kakao.maps.Marker({
-                      map: map,
-                      position: stockTowerPositions[i].latlng,
-                      title: stockTowerPositions[i].title,
-                      image: towerImage,
-                    });
-
-                    window.kakao.maps.event.addListener(stockTower, "click", () => {
-                      checkStockTower(stockTowerPositions[i].id);
-                      setTowerName(stockTowerPositions[i].title);
-                    });
-
-                    stockTower.setMap(map);
-                  }
-                }
-
-                // 스톡몬 마커
-                const stockmonPositions = res?.stockmons.map((stockmon) => ({
-                  id: stockmon.id,
-                  latlng: new window.kakao.maps.LatLng(stockmon.latitude, stockmon.longitude),
-                }));
-
-                if (stockmonPositions) {
-                  for (let i = 0; i < stockmonPositions.length; i++) {
-                    const stockmonImaegSize = new window.kakao.maps.Size(100, 100);
-                    const stockmonImgSrc = `${process.env.NEXT_PUBLIC_S3_URL}/${i}.png`;
-                    const stockmonImage = new window.kakao.maps.MarkerImage(stockmonImgSrc, stockmonImaegSize);
-                    const stockmon = new window.kakao.maps.Marker({
-                      map: map,
-                      position: stockmonPositions[i].latlng,
-                      id: stockmonPositions[i].id,
-                      image: stockmonImage,
-                    });
-
-                    window.kakao.maps.event.addListener(stockmon, "click", () => {
-                      setStockmonId(stockmonPositions[i].id);
-                    });
-
-                    stockmon.setMap(map);
-                  }
-                }
-              });
-            }
-          });
-        } else {
-          // Geolocation API 사용 불가 시 기본 위치 설정
+        // 지도 초기화
+        const initializeMap = (latitude: number, longitude: number) => {
+          const myLocation = new window.kakao.maps.LatLng(latitude, longitude);
           const options = {
-            center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+            center: myLocation,
             level: 2,
             draggable: false,
           };
+
           map = new window.kakao.maps.Map(container, options);
+
+          const userImageSrc = "images/tempPerson.svg";
+          const userImageSize = new window.kakao.maps.Size(100, 100);
+          const imageOption = { offset: new window.kakao.maps.Point(27, 69) };
+          const markerImage = new window.kakao.maps.MarkerImage(userImageSrc, userImageSize, imageOption);
+
+          userMarker = new window.kakao.maps.Marker({
+            position: myLocation,
+            image: markerImage,
+            map: map,
+          });
+        };
+
+        // 사용자 위치 추적 및 지도 업데이트
+        const updateUserLocation = (latitude: number, longitude: number) => {
+          const myLocation = new window.kakao.maps.LatLng(latitude, longitude);
+          // console.log(myLocation);
+          map.setCenter(myLocation);
+          if (userMarker) {
+            userMarker.setPosition(myLocation);
+          }
+        };
+
+        // 버퍼 계산
+        const calcBuffer = (latitude: number, longitude: number) => {
+          const maxLat = latitude + 0.0048;
+          const minLat = latitude - 0.0048;
+          const maxLon = longitude + 0.003;
+          const minLon = longitude - 0.003;
+          buffer = { maxLat, minLat, maxLon, minLon };
+        };
+
+        // 주변 정보 얻어오기
+        const getMapInfo = (latitude: number, longitude: number) =>
+          service.getMapInfo({ latitude, longitude }).then((res) => {
+            console.log(res);
+            // 스톡타워 마커
+            const stockTowerPositions = res?.stockTowers.map((tower) => ({
+              id: tower.id,
+              title: tower.name,
+              latlng: new window.kakao.maps.LatLng(tower.latitude, tower.longitude),
+            }));
+            const towerImageSrc = "images/peachTower.svg";
+
+            if (stockTowerPositions) {
+              for (let i = 0; i < stockTowerPositions.length; i++) {
+                const towerImageSize = new window.kakao.maps.Size(80, 80);
+                const towerImage = new window.kakao.maps.MarkerImage(towerImageSrc, towerImageSize);
+                const stockTower = new window.kakao.maps.Marker({
+                  map: map,
+                  position: stockTowerPositions[i].latlng,
+                  title: stockTowerPositions[i].title,
+                  image: towerImage,
+                });
+
+                window.kakao.maps.event.addListener(stockTower, "click", () => {
+                  checkStockTower(stockTowerPositions[i].id);
+                  setTowerName(stockTowerPositions[i].title);
+                });
+
+                stockTower.setMap(map);
+              }
+            }
+
+            // 스톡몬 마커
+            const stockmonPositions = res?.stockmons.map((stockmon) => ({
+              id: stockmon.stockmon_id,
+              latlng: new window.kakao.maps.LatLng(stockmon.latitude, stockmon.longitude),
+            }));
+
+            if (stockmonPositions) {
+              for (let i = 0; i < stockmonPositions.length; i++) {
+                const stockmonImaegSize = new window.kakao.maps.Size(100, 100);
+                const stockmonImgSrc = `${process.env.NEXT_PUBLIC_S3_URL}/${i}.png`;
+                const stockmonImage = new window.kakao.maps.MarkerImage(stockmonImgSrc, stockmonImaegSize);
+                const stockmon = new window.kakao.maps.Marker({
+                  map: map,
+                  position: stockmonPositions[i].latlng,
+                  id: stockmonPositions[i].id,
+                  image: stockmonImage,
+                });
+
+                window.kakao.maps.event.addListener(stockmon, "click", () => {
+                  setStockmonId(stockmonPositions[i].id);
+                });
+
+                stockmon.setMap(map);
+              }
+            }
+          });
+
+        // 처음엔 getCurrentPosition
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition((position) => {
+            const latitude = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            console.log("log:", longitude);
+            initializeMap(latitude, longitude);
+            calcBuffer(latitude, longitude);
+            getMapInfo(latitude, longitude);
+
+            // 그 후 watchPosition
+            navigator.geolocation.watchPosition((position) => {
+              const newLatitude = position.coords.latitude;
+              const newLongitude = position.coords.longitude;
+              calcBuffer(latitude, longitude);
+              console.log("찍고 있니?", position.coords);
+              // 지도와 마커 업데이트
+              updateUserLocation(newLatitude, newLongitude);
+              console.log(buffer);
+
+              if (
+                newLatitude <= buffer.minLat ||
+                newLatitude >= buffer.maxLat ||
+                newLongitude <= buffer.minLon ||
+                newLongitude >= buffer.maxLon
+              ) {
+                calcBuffer(newLatitude, newLongitude);
+                getMapInfo(latitude, longitude);
+              }
+            });
+          });
+        } else {
+          console.log("Geolocation API를 지원하지 않습니다.");
         }
       });
     };
-
     kakaoMapScript.addEventListener("load", onLoadKakaoAPI);
 
     return () => {
