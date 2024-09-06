848734c278fa192abf965cc878431ff4ab913a43
hyuna <kook980316@gmail.com>
2024-09-06 13:47:03 +0900

    feat: 모바일 환경에서 앱이 깔려있는지 아닌지 확인하는 이벤트 추가

---
+++ src/components/BeforeInstallPrompt.tsx
@@ -0,0 +1,66 @@
+import { Fragment, useEffect, useState } from "react";
+import { BeforeInstallPromptEvent } from "@/types/window";
+import MobileInstallPrompt from "./MobileInstallPrompt";
+
+const defaultBeforeInstallPromptEvent: BeforeInstallPromptEvent = {
+  platforms: [],
+  userChoice: Promise.resolve({ outcome: "dismissed", platform: "" }),
+  preventDefault: () => {},
+  prompt: () => Promise.resolve(),
+};
+
+const isIOSPromptActive = () => {
+  const isActive = JSON.parse(localStorage.getItem("iosInstalled") || "true");
+  if (isActive) {
+    return defaultBeforeInstallPromptEvent;
+  }
+  return null;
+};
+
+export default function BeforeInstallPrompt() {
+  // 기기 파악
+  const isDeviceIOS = /iPad|iPhone|iPod/.test(window.navigator.userAgent);
+  const [deferredPrompt, setDeferredPrompt] = useState<BeforeInstallPromptEvent | null>(
+    isDeviceIOS ? isIOSPromptActive() : defaultBeforeInstallPromptEvent
+  );
+
+  const handleInstallClick = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+
+      deferredPrompt.userChoice.then(() => {
+        setDeferredPrompt(null);
+      });
+    }
+  };
+
+  const handleCancelClick = () => {
+    localStorage.setItem("iosInstalled", "false");
+    setDeferredPrompt(null);
+  };
+
+  const handleBeforeInstallPrompt = (event: BeforeInstallPromptEvent) => {
+    event.preventDefault();
+    setDeferredPrompt(event);
+  };
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+    };
+  }, []);
+
+  return (
+    <Fragment>
+      {deferredPrompt && (
+        <MobileInstallPrompt
+          handleInstallClick={handleInstallClick}
+          handleCancelClick={handleCancelClick}
+          platform={isDeviceIOS ? "ios" : "android"}
+        />
+      )}
+    </Fragment>
+  );
+}
