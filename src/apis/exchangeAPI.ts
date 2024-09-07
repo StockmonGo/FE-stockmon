1f96ed85567f4d12fefdc4dd93c9cbccdf4e1da5
soyeon <uyyu1734@gmail.com>
2024-09-07 15:57:52 +0900

    feat: exchange API추가

---
+++ src/apis/exchangeAPI.ts
@@ -0,0 +1,60 @@
+import { IExchangeRequest } from "@/types/exchanges";
+import { BaseApi, handleApiError } from "./baseAPI";
+import { SuccessResponse } from "@/types/SuccessResponse";
+
+const EXCHANGE_BASE_URL = "/api/core/stockmons/exchange";
+export default class exchangeAPI extends BaseApi {
+  // 교환 요청 목록 조회 API
+  async getExchangeRequests(): Promise<IExchangeRequest[] | null> {
+    try {
+      const response = await this.fetcher.get(EXCHANGE_BASE_URL + "/request");
+      return response.data.stockmons;
+    } catch (error) {
+      handleApiError(error);
+    }
+  }
+
+  // 교환 요청
+  async requestExchange(
+    travelerId: number,
+    travelerStockmonId: number
+  ): Promise<SuccessResponse<null>> {
+    try {
+      const response = await this.fetcher.post(EXCHANGE_BASE_URL + "/request", {
+        travelerId,
+        travelerStockmonId,
+      });
+      return new SuccessResponse(response.status, "요청 성공", null);
+    } catch (error) {
+      handleApiError(error);
+    }
+  }
+
+  // 교환 요청 수락
+  async acceptExchange(
+    noticeId: number,
+    travelerStockmonId: number
+  ): Promise<SuccessResponse<null>> {
+    try {
+      const response = await this.fetcher.post(EXCHANGE_BASE_URL + "/accept", {
+        noticeId,
+        travelerStockmonId,
+      });
+      return new SuccessResponse(response.status, "수락 성공", null);
+    } catch (error) {
+      handleApiError(error);
+    }
+  }
+
+  // 교환 요청 거절
+  async rejectExchange(noticeId: number): Promise<SuccessResponse<null>> {
+    try {
+      const response = await this.fetcher.post(EXCHANGE_BASE_URL + "/reject", {
+        noticeId,
+      });
+      return new SuccessResponse(response.status, "거절 성공", null);
+    } catch (error) {
+      handleApiError(error);
+    }
+  }
+}
