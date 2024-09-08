13a4063d3a4bc3fbdf1d6da30a7446d4888f37fb
soyeon <uyyu1734@gmail.com>
2024-09-08 18:26:47 +0900

    feat: 유저조회&동맹 요청
    모달타입 타입폴더로 뺌
    getUser auth -> allaince로 이동(동맹에서만 사용되어서)
    add,openExchange에 닉네임도 추가해놓음

--- src/app/alliances/page.tsx
+++ src/app/alliances/page.tsx
@@ -4,13 +4,12 @@ import AllianceItem from "@/components/ui/alliance/AllianceItem";
 import Modal from "@/components/ui/Modal";
 import SearchBar from "@/components/ui/SearchBar";
 import React, { useEffect, useMemo, useState } from "react";
-import dummyAlliances from "@/../dummy/alliance/alliances.json";
-import dummyTraveler from "@/../dummy/alliance/searchTraveler.json";
 import StockExchangeModal from "@/components/ui/message/StockExchangeModal";
 import { IStockmonInfo } from "../message/page";
 import allianceAPI from "@/apis/allianceAPI";
 import useSWR from "swr";
 import { ITraveler } from "@/types/member";
+import { IModal } from "@/types/modal";
 
 export interface IStockExchangeInfo {
   travelerId?: number;
@@ -25,11 +24,23 @@ export default function Alliance() {
   const { data, error } = useSWR<ITraveler[]>("alliance", () =>
     service.getAlliances()
   );
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  //TODO: 커스텀훅으로 뺼까?
   const [stockExchange, setStockExchange] = useState<IStockExchangeModalInfo>({
     isOpen: false,
   });
+  const [modal, setModal] = useState<IModal>({
+    isOpen: false,
+    content: "",
+    title: "",
+    onClick: () => {},
+  });
+
+  const closeModal = () => {
+    setModal({
+      isOpen: false,
+      content: "",
+      onClick: () => {},
+    });
+  };
 
   const openExchange = (travelerId: number) => {
     //exchange에 필요한 데이터 세팅해서 모달 오픈
@@ -38,28 +49,67 @@ export default function Alliance() {
       travelerId,
     });
   };
-  const addAlliance = (travelerId: number) => {
-    //TODO: 동맹추가 모달 열고 확인시 진행
-  };
-  const deleteAlliance = (travelerId: number) => {
-    //TODO: 동맹삭제 모달 열고 확인시 진행
-  };
-  const closeExchange = () => {
-    setStockExchange({ isOpen: false });
-  };
-  const [alliances, setAlliances] = useState<ITraveler[]>(
-    dummyAlliances.data.alliances
-  );
-
-  const [searchedTraveler, setSearchedTraveler] = useState<ITraveler>();
-  const [searchKeyword, setSearchKeyword] = useState<string>("");
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchKeyword(event.target.value);
-  };
 
   const handleConfirmStockExchange = (stockmonId: number) => {
     //TODO: 동맹에게 교환요청 해당 스톡몬 보내기
   };
+
+  const closeExchange = () => {
+    setStockExchange({ isOpen: false });
+  };
+
+  const [searchedTraveler, setSearchedTraveler] = useState<ITraveler | null>();
+  const [searchKeyword, setSearchKeyword] = useState<string>("");
+  const [debouncedQuery, setDebouncedQuery] = useState(searchKeyword);
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchKeyword(event.target.value);
+  };
+
+  const searchUser = async () => {
+    try {
+      const res = await service.getUser(debouncedQuery);
+      if (res)
+        setSearchedTraveler({
+          nickname: res.nickname,
+          travelerId: res.travelerId,
+        });
+    } catch (error) {
+      console.log("검색결과가 없습니다", debouncedQuery);
+      setSearchedTraveler(null);
+    }
+  };
+  const addAlliance = async (nickname: string, travelerId: number) => {
+    try {
+      const res = await service.requestAlliance(travelerId);
+      if (res) {
+        setModal({
+          isOpen: true,
+          title: "동맹 요청",
+          content: `${nickname}님께 동맹을 요청했습니다.`,
+          onClick: closeModal,
+        });
+      }
+    } catch (error: any) {
+      setModal({
+        isOpen: true,
+        title: "동맹 요청 실패",
+        content: error.toString(),
+        onClick: closeModal,
+      });
+    }
+  };
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedQuery(searchKeyword);
+    }, 500); // 500ms 후에 검색 쿼리 업데이트
+    return () => clearTimeout(timerId);
+  }, [searchKeyword]);
+
+  useEffect(() => {
+    if (debouncedQuery) searchUser();
+  }, [debouncedQuery]);
+
   return (
     <>
       <SearchBar
@@ -70,20 +120,20 @@ export default function Alliance() {
         className={`w-full bg-stock-blue-200 bg-border-custom-dotted h-4/5 rounded-lg p-3 mt-6`}
       >
         <div className="w-full bg-stock-lemon-50 h-full rounded-lg p-2 overflow-scroll">
-          {data &&
+          {!searchKeyword &&
+            data &&
             data.map((data, idx) => {
               return (
                 <AllianceItem
                   key={data.travelerId}
                   traveler={data}
                   addAlliance={addAlliance}
-                  deleteAlliance={deleteAlliance}
                   openExchange={openExchange}
                   travelerType={"alliance"}
                 />
               );
             })}
-          {alliances && alliances.length === 0 && (
+          {!searchKeyword && data && data.length === 0 && (
             <div className="flex items-center flex-col justify-center h-full">
               <img
                 className="text-stock-blue-400"
@@ -95,22 +145,33 @@ export default function Alliance() {
               </p>
             </div>
           )}
+          {searchedTraveler === null && (
+            <div className="flex items-center flex-col justify-center h-full">
+              <img
+                className="text-stock-blue-400"
+                src="/images/cannot-find.png"
+                alt="찾을 수 없음"
+              />
+              <p className="font-ptr text-lg text-stock-blue-400">
+                해당 유저가 없습니다
+              </p>
+            </div>
+          )}
           {searchedTraveler && (
             <AllianceItem
               traveler={searchedTraveler}
               travelerType={"others"}
               addAlliance={addAlliance}
-              deleteAlliance={deleteAlliance}
               openExchange={openExchange}
             />
           )}
         </div>
       </div>
       <Modal
-        open={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        onConfirm={() => setIsModalOpen(false)}
-        hasClose
+        open={modal.isOpen}
+        onClose={modal.onClick}
+        title={modal.title}
+        describe={modal.content}
       />
       <StockExchangeModal
         open={stockExchange.isOpen}
