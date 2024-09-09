98a85177f9a65ebbe59be4599e5c4454750e4de0
hyuna <kook980316@gmail.com>
2024-09-09 16:25:07 +0900

    feat: 튜토리얼 페이지 추가

---
+++ src/app/tutorial/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import Button from "@/components/ui/Button";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function Tutorial() {
+  const [step, setStep] = useState(1);
+  const router = useRouter();
+  const nextStep = () => {
+    if (step == 5) {
+      router.push("/world");
+      return;
+    }
+    setStep(step + 1);
+  };
+  const skipTutorial = () => {
+    router.push("/world");
+  };
+  const generateImageUrls = () => {
+    const urls: Record<number, string> = {};
+    for (let i = 1; i <= 5; i++) {
+      urls[i] = `/images/tutorial${i}.svg`;
+    }
+    return urls;
+  };
+  const imgs = generateImageUrls();
+
+  return (
+    <div className="w-screen h-screen bg-[url('/images/bg.jpg')] relative grid justify-items-center">
+      <div className="absolute right-6 top-6 h-fit w-fit">
+        <Button onClick={nextStep} text={step === 5 ? "시작하기" : "다음으로"} />
+      </div>
+      <img src={imgs[step]} className="max-h-screen w-full h-full object-cover" />
+      <div className="absolute top-6 left-6 h-fit w-fit">
+        <Button onClick={skipTutorial} text="스킵하기" />
+      </div>
+    </div>
+  );
+}
