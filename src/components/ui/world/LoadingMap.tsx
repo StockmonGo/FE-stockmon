15acfd76ab36f5310ecfcf14bc655d5ea37463ab
KimRiun <kimriun27@gmail.com>
2024-09-09 17:04:27 +0900

    design: 지도 로드 전 로딩 화면 구현

---
+++ src/components/ui/world/LoadingMap.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./loading-map.css";
+
+export default function LoadingMap() {
+  return (
+    <div className="loader loader-41 w-full h-full">
+      <div className="loader-container">
+        <p className="loader-title-double">Loading</p>
+        <p className="loader-title">Loading</p>
+      </div>
+    </div>
+  );
+}
