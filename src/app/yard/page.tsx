5dffddc2130b3a21fb3c735217ff821f86cf12e9
hyuna <kook980316@gmail.com>
2024-09-10 13:27:35 +0900

    design: yard 가운데 정렬

--- src/app/yard/page.tsx
+++ src/app/yard/page.tsx
@@ -20,19 +20,21 @@ export default function Yard() {
   }
 
   return (
-    <div className="w-screen max-w-lg h-screen bg-[url('/images/yardBg.png')] bg-cover bg-no-repeat relative">
-      {data?.stockmons.map((item) => {
-        const top = getRandomPosition(500);
-        const left = getRandomPosition(300);
+    <div className="w-full h-full flex justify-center">
+      <div className="w-screen max-w-lg h-screen bg-[url('/images/yardBg.png')] bg-cover bg-no-repeat relative flex justify-center">
+        {data?.stockmons.map((item) => {
+          const top = getRandomPosition(500);
+          const left = getRandomPosition(300);
 
-        return (
-          <div key={item.id} style={{ top, left }} className="bounce w-fit h-fit absolute grid justify-items-center">
-            <StockmonFeel stockmon={item} />
-          </div>
-        );
-      })}
-      <div className="fixed bottom-6 w-full flex justify-center">
-        <BtnClose />
+          return (
+            <div key={item.id} style={{ top, left }} className="bounce w-fit h-fit absolute grid justify-items-center">
+              <StockmonFeel stockmon={item} />
+            </div>
+          );
+        })}
+        <div className="fixed bottom-6">
+          <BtnClose />
+        </div>
       </div>
     </div>
   );
