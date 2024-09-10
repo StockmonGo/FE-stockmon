4d91891c5c0e591ff5bbe4b0096563c4b65740be
soyeon <uyyu1734@gmail.com>
2024-09-10 22:20:02 +0900

    design: 반짝이 뒤에 더 추가

--- src/components/ui/Twinkle.tsx
+++ src/components/ui/Twinkle.tsx
@@ -2,52 +2,101 @@ import React from "react";
 
 export default function Twinkle() {
   return (
-    <div className="stars-container z-0">
-      <div className="stars">
-        <div></div>
-        <div></div>
-        <div></div>
+    <>
+      <div className="stars-container z-0">
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
-      <div className="stars">
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="stars-container z-0" style={{ top: "100%", left: "95%" }}>
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="stars">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
-      <div className="stars">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div className="stars">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div className="stars">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div className="stars">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div className="stars">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div className="stars">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div className="stars">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </div>
+    </>
   );
 }
