0959d8b039045c222627db25a8f75288c8aadb04
hyuna <kook980316@gmail.com>
2024-09-10 20:45:39 +0900

    fix: stockmons 빈 배열 올 때 처리

--- src/app/yard/page.tsx
+++ src/app/yard/page.tsx
@@ -22,16 +22,18 @@ export default function Yard() {
   return (
     <div className="w-full h-screen-small flex justify-center">
       <div className="w-screen max-w-xl h-full bg-[url('/images/yardBg.png')] bg-cover bg-no-repeat relative flex justify-center">
-        {data?.stockmons.map((item) => {
-          const top = getRandomPosition(500);
-          const left = getRandomPosition(300);
+        {data.stockmons &&
+          data.stockmons.length > 0 &&
+          data.stockmons.map((item) => {
+            const top = getRandomPosition(500);
+            const left = getRandomPosition(300);
 
-          return (
-            <div key={item.id} style={{ top, left }} className="bounce w-fit absolute grid justify-items-center">
-              <StockmonFeel stockmon={item} />
-            </div>
-          );
-        })}
+            return (
+              <div key={item.id} style={{ top, left }} className="bounce w-fit absolute grid justify-items-center">
+                <StockmonFeel stockmon={item} />
+              </div>
+            );
+          })}
         <div className="fixed bottom-6">
           <BtnClose />
         </div>
