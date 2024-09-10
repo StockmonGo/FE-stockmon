780a264addea159ce22afef25aeaf32821440846
KimRiun <kimriun27@gmail.com>
2024-09-10 10:25:43 +0900

    design: 태그 폰트 크기 수정

--- src/components/ui/books/StockTag.tsx
+++ src/components/ui/books/StockTag.tsx
@@ -8,8 +8,8 @@ type Props = {
 export default function StockTag({ content, category }: Props) {
   return (
     <div className="flex items-center gap-1 px-2 py-1 rounded-lg bg-[#7779FF] text-white ">
-      <p className=" font-ptb ">{content}</p>
-      <p className=" font-ptr">{category}</p>
+      <p className=" text-sm font-ptb ">{content}</p>
+      <p className=" text-sm font-ptr">{category}</p>
     </div>
   );
 }
